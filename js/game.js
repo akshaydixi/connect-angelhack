0c597df7c32e2800a82999b5dfa7c9768f85df3a
akshaydixi <akshaydixi@gmail.com>
2013-06-23 06:58:00 +0000

    game updated

--- js/game.js
+++ js/game.js
@@ -10,8 +10,17 @@ function Game(){
 Game.prototype.startGame = function(utility,io,connect){
 	var stopwatch = new Stopwatch();
 	var gameObject = this;
+	this.creator = gameObject.chooseCreator(connect);
+	utility.sendEventToConnectInPlay('creatorChosen',{chosenCreatorID : this.creator.id},io,connect);
+	
 	//Game logic set
 	//TODO :Send emits and call events
 	
 };
 
+
+Game.prototype.chooseCreator = function(connect){
+	var length = connect.players.length;
+	var index = Math.floor(Math.random()*length);
+	connect.players[index].setCreator();
+};
